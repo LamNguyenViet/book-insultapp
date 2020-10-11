129b906b89b0eb02fb51d0b8c4811351640608e0
LamNguyenViet <34784624+LamNguyenViet@users.noreply.github.com>
2020-10-11 15:23:00 +0700

    Lam Nguyen Viet

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,5 +1,6 @@
 package org.openshift;
 
+import java.sql.*;
 import java.util.Random;
 
 public class InsultGenerator {
@@ -7,13 +8,32 @@ public class InsultGenerator {
 		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
 		String vowels = "AEIOU";
 		String article = "an";
-		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
-		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
-		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
+		String theInsult = "";
+		try {
+			String databaseURL = "jdbc:postgresql://172.30.156.227/insults";
+			String username = "insult";
+			String password = "insult";
+			Connection connection = DriverManager.getConnection(databaseURL, username, password);
+			if(connection != null){
+				String sql = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				while (rs.next()){
+					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1){
+						article = "a";
+					}
+					theInsult = String.format("Thou art %s %s %s %s!", article,
+							rs.getString("first"), rs.getString("second"),
+							rs.getString("noun"));
+				}
+				rs.close();
+				connection.close();
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+			return "Database connection problem";
 		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
+		return theInsult;
 	}
 
 }
